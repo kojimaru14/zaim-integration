c3cb9c20fdd4c3c6a0391a2128bdb46750eb1c3a
jiko <>
2021-11-21 20:31:51 +0900

    Fix "Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch" on Heroku

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+export DJANGO_SETTINGS_MODULE=config.settings.prod
 # https://help.heroku.com/YNG7SACF/how-do-i-define-a-start-command-for-an-app-using-docker-images
 if [[ $DYNO == "web"* ]]; then
-  python manage.py runserver --settings=config.settings.prod
+  python manage.py runserver 0.0.0.0:$PORT
   echo "Zaim web process started"
 elif  [[ $DYNO == "worker"* ]]; then
-  export DJANGO_SETTINGS_MODULE=config.settings.prod
   celery -A config.celery worker --loglevel=INFO
   echo "Zaim worker process started"
 fi
