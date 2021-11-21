e8bbfff3a0f4fe39809d507e2d155b644376f955
jiko <>
2021-11-21 18:19:28 +0900

    Fix the error "/bin/sh: 1: start.sh: not found" when the container process was started

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
-# start.sh
+#!/bin/bash
+
 # https://help.heroku.com/YNG7SACF/how-do-i-define-a-start-command-for-an-app-using-docker-images
 if [[ $DYNO == "web"* ]]; then
   python manage.py runserver --settings=config.settings.prod
+  echo "Zaim web process started"
 elif  [[ $DYNO == "worker"* ]]; then
   celery -A config.celery worker --loglevel=INFO
+  echo "Zaim worker process started"
 fi
