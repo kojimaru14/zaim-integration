48655dbcd856c1746e74f14c57b29d493cd88565
jiko <>
2021-12-05 11:49:56 +0900

    On prod, only run migration upon release

--- start.sh
+++ start.sh
@@ -7,10 +7,14 @@ if [ -f /code/service_account.json ]; then
   echo ${GOOGLE_CREDENTIALS} > /code/service_account.json
 fi
 
-python manage.py migrate
+echo $DYNO
 
 # https://help.heroku.com/YNG7SACF/how-do-i-define-a-start-command-for-an-app-using-docker-images
 if [[ $DYNO == "web"* ]]; then
+  if [[ "$DYNO" =~ ^release.* ]]; then # Run migration only on release phase (Ref: https://stackoverflow.com/questions/59537030/release-phase-with-container-registry-on-heroku-runs-container#answer-59537345)
+    set -e
+    python manage.py migrate
+  fi
   gunicorn config.wsgi --log-file -
   echo "Zaim web process started"
 elif  [[ $DYNO == "worker"* ]]; then
