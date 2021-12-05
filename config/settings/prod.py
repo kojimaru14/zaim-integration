392730740afc92a1949f0624586867c9acf86a2b
jiko <>
2021-12-05 12:25:12 +0900

    Use sqlite in test (TravisCI) as DATABASE_URL is only defined in Heroku (not on Travis)

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,8 +7,9 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1','.herokuapp.com']
 CELERY_BROKER_URL = os.environ.get('REDIS_URL')
 CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+if( os.getenv('DATABASE_URL') ): # If DATABASE_URL is defined, use it. If not, the default sqlite is used (which is the case when test is run via TravisCI)
+  import dj_database_url
+  DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # For test suites (ENV variables imported from travis-ci)
 ZAIM_USER = os.environ.get('ZAIM_USER')
