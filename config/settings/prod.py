fe500ccd87e76942d09eb2fb60ca0791f4413161
jiko <>
2021-12-05 13:33:18 +0900

    Set DEBUG=False

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,13 +1,13 @@
 from .base import *
 import django_heroku
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['localhost','127.0.0.1','.herokuapp.com']
 
 CELERY_BROKER_URL = os.environ.get('REDIS_URL')
 CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 
-if( os.getenv('DATABASE_URL') ): # If DATABASE_URL is defined, use it. If not, the default sqlite is used (which is the case when test is run via TravisCI)
+if( os.environ.get('DATABASE_URL') ): # If DATABASE_URL is defined, use it. If not, the default sqlite is used (which is the case when test is run via TravisCI)
   import dj_database_url
   DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
