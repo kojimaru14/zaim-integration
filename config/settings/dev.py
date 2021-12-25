3bcfd00275b99d419148689879f9f0437b1fe813
jiko <>
2021-12-26 07:56:34 +0900

    Use postgres container only when an env variable is defined (i.e. run via docker-compose.yml)

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -7,16 +7,17 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1','.herokuapp.com']
 CELERY_BROKER_URL = 'redis://redis:6379'
 CELERY_RESULT_BACKEND = 'redis://redis:6379'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+if os.getenv("DB") == "postgres":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
     }
-}
 
 try:
     from .local_settings import *
