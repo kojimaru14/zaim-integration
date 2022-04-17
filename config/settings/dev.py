9e99d9955ff3f44d0999b08ae424cb5dad8c3fe4
jiko <>
2022-04-17 16:54:01 +0900

    Stop using postgresql on Dev environment, and use sqlite3.db instead

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -7,17 +7,8 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1','.herokuapp.com']
 CELERY_BROKER_URL = 'redis://redis:6379'
 CELERY_RESULT_BACKEND = 'redis://redis:6379'
 
-if os.getenv("DB") == "postgres":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': 'db',
-            'PORT': 5432,
-        }
-    }
+from django.conf import settings
+print( settings.DATABASES['default'] )
 
 try:
     from .local_settings import *
