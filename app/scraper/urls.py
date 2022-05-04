016e0c02f926a881c985593152b6abc1e9b86888
jiko <>
2022-05-04 10:00:54 +0900

    Set trailing_slash=False on /api route

--- app/scraper/urls.py
+++ app/scraper/urls.py
@@ -4,15 +4,16 @@ from rest_framework import routers
 
 app_name = 'zaim'
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
+# router = routers.DefaultRouter()
 router.register('users', views.UserViewSet)
 router.register('rakuten', views.RakutenViewSet)
 router.register('zaim', views.ZaimViewSet)
 
 urlpatterns = [
     # path('', views.hello_world, name="index"),
-    path('', include(router.urls)),
+    path('', include((router.urls, app_name))),
     path('run', views.run_task, name="run_task"),
-    path('rakuten', views.run_rakuten_task, name="run_rakuten"),
+    # path('rakuten', views.run_rakuten_task, name="run_rakuten"),
     path('tasks', views.list_tasks, name="list_tasks")
 ]
